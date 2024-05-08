729900e8c22fd99982b2bf33cb098413af7b7773
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-05-08 11:53:47 +0200

    fix and update code

--- ES6_classes/4-pricing.js
+++ ES6_classes/4-pricing.js
@@ -1,31 +1,31 @@
 /*eslint-disable*/
 export default class Pricing {
   constructor(amount, currency) {
-    this._amount = amount;
-    this._currency = currency;
-  }
-
-  get amount() {
-    return this._amount;
+      this._amount = amount;
+      this._currency = currency;
   }
 
   set amount(amount) {
-    this._amount = amount;
+      this._amount = amount;
   }
 
-  get currency() {
-    return this._currency;
+  get amount() {
+      return this._amount;
   }
 
   set currency(currency) {
-    this._currency = currency;
+      this._currency = currency;
+  }
+
+  get currency() {
+      return this._currency;
   }
 
   displayFullPrice() {
-    return `${this._amount} ${this._currency.displayFullCurrency()}`;
+      return `${this.amount} ${this.currency.displayFullCurrency()}`;
   }
 
   static convertPrice(amount, conversionRate) {
-    return amount * conversionRate;
+      return amount * conversionRate;
   }
 }
