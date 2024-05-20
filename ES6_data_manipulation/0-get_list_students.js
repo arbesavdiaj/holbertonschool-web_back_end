43e876fa59e53fb9698f8000bf08c38651bcbfeb
arbesavdiaj <arbesavdiaj14@gmail.com>
2024-05-20 09:38:34 +0200

    basic list of objects

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,19 +1,19 @@
 /*eslint-disable*/
 const getListStudents = () => [
-    {
-        id: 1,
-        firstName: 'Guillame',
-        location: 'San Francisco',
-    },
-    {
-        id: 2,
-        firstName: 'James',
-        location: 'Columbia',
-    },
-    {
-        id: 5,
-        firstName: 'Serena',
-        location: 'San Francisco',
-    },
+  {
+    id: 1,
+    firstName: "Guillame",
+    location: "San Francisco",
+  },
+  {
+    id: 2,
+    firstName: "James",
+    location: "Columbia",
+  },
+  {
+    id: 5,
+    firstName: "Serena",
+    location: "San Francisco",
+  },
 ];
 export default getListStudents;
